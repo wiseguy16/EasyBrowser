0cacbcbd990341aa6e8c7ac96fe0e8c80076095a
wiseguy16 <gergusa04@gmail.com>
2016-08-24 12:12:36 -0400

    initial commit

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func openPageInSafari(_ sender: UIButton)
+    {
+        // open http://developer.apple.com
+
+    }
 
 }
 
